692aa5976446992a3a001eb9c850f5d3292147cc
Serhii Boiko <boyko0043@gmail.com>
2020-08-05 12:33:51 +0300

    Updates

---
+++ precache-manifest.7893c372ba385a43bb56d0bc52a9c016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fff27c72c76926ea23c15c03c415bf",
+    "url": "/react_warriors/index.html"
+  },
+  {
+    "revision": "5e43dcff9da39b250a80",
+    "url": "/react_warriors/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5e43dcff9da39b250a80",
+    "url": "/react_warriors/static/js/2.b8774d21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_warriors/static/js/2.b8774d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08815fe611cf8275ab6",
+    "url": "/react_warriors/static/js/main.9f5d43c3.chunk.js"
+  },
+  {
+    "revision": "a6b998a5983a7a9c3b5b",
+    "url": "/react_warriors/static/js/runtime-main.92a83727.js"
+  }
+]);
